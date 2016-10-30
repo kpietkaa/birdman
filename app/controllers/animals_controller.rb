d3ee2016d66e7d1cb4a911efbdbb22b1be56a758
kpietkaa <kpietkaa@gmail.com>
2016-10-31 00:15:14 +0100

    Add new, create actions to animals

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,24 @@
 class AnimalsController < ApplicationController
   def index
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to @animal, notice: 'Animal has been created'
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :animal_type, :breed, :sex, :castration, :birth_date)
+  end
 end
