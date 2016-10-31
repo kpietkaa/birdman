46cb1deb651b9c8d4a295109c0ef5703e8f47d1d
kpietkaa <kpietkaa@gmail.com>
2016-10-31 10:14:40 +0100

    Add edit & update actions + edit form

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,9 +1,9 @@
 class AnimalsController < ApplicationController
+  before_action :find_animal, only: [:show, :edit, :update, :destroy]
   def index
   end
 
   def show
-    @animal = Animal.find(params[:id])
   end
 
   def new
@@ -19,10 +19,25 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def edit
+    @animals = Animal.animal_types.map{ |key, val| [ key.split('_').first.capitalize, key ] }
+  end
+
+  def update
+    if @animal.update(animal_params)
+      redirect_to @animal, notice: 'Animal has been updated'
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def animal_params
     params.require(:animal).permit(:name, :animal_type, :breed, :sex, :castration, :birth_date)
+  end
+
+  def find_animal
+    @animal = Animal.find(params[:id])
   end
 end
