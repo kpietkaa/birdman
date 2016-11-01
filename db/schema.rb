99b19904b9405b695804ea91fca40d946b47605b
kpietkaa <kpietkaa@gmail.com>
2016-11-01 18:42:06 +0100

    Add Event model, controller and routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030230004) do
+ActiveRecord::Schema.define(version: 20161101173844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20161030230004) do
     t.date     "birth_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.string   "event_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
---
+++ spec/factories/events.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :event do
+    title "MyString"
+    start_at "2016-11-01 18:38:44"
+    end_at "2016-11-01 18:38:44"
+    event_type "MyString"
+  end
+end
