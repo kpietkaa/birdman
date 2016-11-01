3f96b7a9bda7e276622f5fda8c4630a071cad2c3
kpietkaa <kpietkaa@gmail.com>
2016-11-01 19:49:01 +0100

    Fix typos

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ActiveRecord::Base
   validates :title, presence: true
-  attr_accesor :date_range
+  attr_accessor :date_range
 
   def all_day_event?
-    self.start_at == selft.start_at.midnight && self.end_at == self.end_at.midnight ? true : false
+    self.start_at == self.start_at.midnight && self.end_at == self.end_at.midnight ? true : false
   end
 end
