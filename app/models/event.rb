ed758f2aff909c34b0cb4134537838a83cf24455
kpietkaa <kpietkaa@gmail.com>
2016-11-01 18:57:58 +0100

    Add method if event is all day long and title validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
+  validates :title, presence: true
   attr_accesor :date_range
+
+  def all_day_event?
+    self.start_at == selft.start_at.midnight && self.end_at == self.end_at.midnight ? true : false
+  end
 end
