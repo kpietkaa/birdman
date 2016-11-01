4c269fe408398d4f10c45248c43f0f9d67bc89b5
kpietkaa <kpietkaa@gmail.com>
2016-11-01 19:58:54 +0100

    Fix full_calendar typo

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -15,11 +15,8 @@ initialize_calendar = function() {
 
       // Create event
       select: function(start, end) {
-        $.getScript('event/new', function() {
+        $.getScript('events/new', function() {
           $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
-          date_range_picker();
-          $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
-          $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
         });
 
         calendar.fullCalendar('unselect');
