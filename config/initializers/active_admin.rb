741437b3af00e01d450e853226201b52c14090d5
kpietkaa <kpietkaa@gmail.com>
2016-11-03 21:02:04 +0100

    Fix bug

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.setup do |config|
   config.site_title = "Birdman"
-  config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = :authenticate_active_admin_user!
   config.current_user_method = :current_user
   config.logout_link_path = :destroy_user_session_path
   config.batch_actions = true
