cd7534dcf54937c78553e89d4fbdac432706f20b
kpietkaa <kpietkaa@gmail.com>
2016-11-03 13:34:12 +0100

    ActiveAdmin install files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :animals
   resources :events
   get '/visitors', to: "visitors#index"
+  root to: "animals#index"
 end
