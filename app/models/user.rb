363bc9476b9726d314ad2cd90d64783e1e9ca5f7
kpietkaa <kpietkaa@gmail.com>
2016-11-04 14:01:24 +0100

    Add default role to new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :set_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,5 +7,12 @@ class User < ActiveRecord::Base
 
   def role?(r)
     role.include? r.to_s
+  end
+
+  private
+  def set_default_role
+    self.role ||= 'user'
+    binding.pry
+  
   end
 end
