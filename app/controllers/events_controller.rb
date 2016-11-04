cdb3f611c3137cb28034a308929b38529dfc0bde
kpietkaa <kpietkaa@gmail.com>
2016-11-04 21:56:04 +0100

    Add create event with user owner

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,12 +8,12 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = Event.new
+    @event = current_user.events.build
     authorize! :new, @event
   end
 
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
     change_range_to_date
     @event.save
   end
@@ -38,7 +38,7 @@ class EventsController < ApplicationController
   end
 
   def find_event
-    @event = Event.find(params[:id])
+    @event = Event.where(user_id: current_user.id).find(params[:id])
   end
 
   def change_range_to_date
