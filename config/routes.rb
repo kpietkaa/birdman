de8e904657ae013042e3e38993a23ba1cfeb1859
kpietkaa <kpietkaa@gmail.com>
2016-11-05 14:51:17 +0100

    Move files to existing Visitors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'creatives/index'
-  # devise_for :users
   devise_for :users, controllers: { registrations: 'registrations'}
   ActiveAdmin.routes(self)
   resources :animals
   resources :events
-  get '/visitors', to: "visitors#index"
-  root to: "animals#index"
+  authenticated :user do
+    root to: "animals#index", as: :authenticated_root
+  end
+  root to: "visitors#index"
 end
