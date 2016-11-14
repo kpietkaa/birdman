47ed119f241bfaf6b97973fc424cb77e2a4487e5
kpietkaa <kpietkaa@gmail.com>
2016-11-14 19:23:14 +0100

    Add necessary info's for new history

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,4 +1,43 @@
 class HistoriesController < ApplicationController
   load_and_authorize_resource
 
+  def index
+  end
+
+  def show
+  end
+
+
+  def new
+    @event = Event.find(params[:event_id])
+    @animal_info = Animal.find(@event.animal_id)
+    @owner_info = User.find(@event.user_id)
+    @owner_address = Address.find(@owner_info.address_id)
+    create_event
+    @history = History.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @history = @event.histories.create(history_params)
+    if @history.errors.any?
+      render 'new'
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def history_params
+    params[:history].permit(:description)
+  end
+
+  def create_event
+    visit_hash = Hash[VisitType.all.map{ |v| [v.title, v.color] }]
+    animals_hash = Hash[Animal.all.map{ |a| [ a.name, a.id ] }]
+    animal_owner = Hash[User.all.map{ |u| [ u.full_name, u.id ] }]
+    @event.event_type = visit_hash.key(@event.event_type)
+    @event.animal_name = animals_hash.key(@event.animal_id)
+    @event.owner_name = animal_owner.key(@event.user_id)
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ActiveRecord::Base
   has_one :user
+
+  def street_address
+    self.street + ' ' + self.street_number.to_s + '/' + (self.house_number.empty? ? '' : self.house_number.to_s)
+  end
+
+  def zip_city
+    self.zip_code + ' ' + self.city
+  end
 end
