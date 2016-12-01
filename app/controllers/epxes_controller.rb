53b618b7b208fad1a87797e521cc0a4171f3d925
kpietkaa <kpietkaa@gmail.com>
2016-12-01 22:09:39 +0100

    Add URL fetching

--- app/controllers/epxes_controller.rb
+++ app/controllers/epxes_controller.rb
@@ -1,3 +1,10 @@
 class EpxesController < ApplicationController
 
+  private
+
+  def fetch(url)
+    response = Net::HTTP.get_response(URI.parse(url))
+    data = response.body
+    JSON.parse(data)
+  end
 end
