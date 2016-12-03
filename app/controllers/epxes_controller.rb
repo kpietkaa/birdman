830465d8f48a0a920bed4ead5e8fb2dcd9413430
kpietkaa <kpietkaa@gmail.com>
2016-12-03 19:20:48 +0100

    Add errors handling to controller

--- app/controllers/epxes_controller.rb
+++ app/controllers/epxes_controller.rb
@@ -2,31 +2,46 @@ class EpxesController < ApplicationController
 
   def index
     @cages = Array.new
-    url = "http://zpiiotdiscovery.repository-api.epx-platform.org/api/models/"
     # (2560..2564).map do |i|
-    (2560...2562).map do |i|
+    (2560..2560).map do |i|
+      url = "http://zpiiotdiscovery.repository-api.epx-platform.org/api/models/"
       cage = Cage.new
       url += i.to_s
-      json = fetch url
-      cage.id = json["Id"]
-      cage.name = json["Name"]
-      cage.type = json["RootModelElement"]["Children"][1]["Value"]
-      cage.animal_id = json["RootModelElement"]["Children"][2]["Children"][0]["Children"][0]["Value"]
-      cage.animal_name = json["RootModelElement"]["Children"][2]["Children"][0]["Children"][1]["Value"]
+
+      begin
+        json = fetch url
+        if json["Message"].nil?
+          cage.id = json["Id"]
+          cage.name = json["Name"]
+          cage.type = json["RootModelElement"]["Children"][1]["Value"]
+          cage.animal_id = json["RootModelElement"]["Children"][2]["Children"][0]["Children"][0]["Value"]
+          cage.animal_name = json["RootModelElement"]["Children"][2]["Children"][0]["Children"][1]["Value"]
+        else
+          cage.error = json["Message"]
+        end
+      rescue Timeout::Error
+        cage.error = "Timeout error"
+      end
+
       @cages.push(cage)
     end
 
     @sensors = Array.new
     # (2555..2559).map do |i|
-    (2555..2556).map do |i|
+    (2555..2555).map do |i|
+    url = "http://zpiiotdiscovery.repository-api.epx-platform.org/api/models/"
       sensor = Sensor.new
       url += i.to_s
+      begin
       json = fetch(url)
       sensor.id = json["Id"]
       sensor.name = json["Name"]
       sensor.temperature = json["RootModelElement"]["Children"][0]["Value"]
       sensor.animal_id = json["RootModelElement"]["Children"][2]["Children"][0]["Children"][0]["Value"]
       sensor.animal_name = json["RootModelElement"]["Children"][2]["Children"][0]["Children"][1]["Value"]
+    rescue Timeout::Error
+      sensor.error = "Timeout error"
+    end
       @sensors.push(sensor)
     end
 
