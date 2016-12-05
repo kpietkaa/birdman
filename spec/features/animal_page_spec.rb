6080df7963efe9afc5bc2cde4ac246ba02bf129b
kpietkaa <kpietkaa@gmail.com>
2016-12-05 18:30:38 +0100

    Add tests for animal show

---
+++ spec/features/animal_page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require_relative '../support/feature_helpers'
+require "cancan/matchers"
+
+feature "animal page" do
+  scenario "user's animal page" do
+    sign_in_admin
+
+    animal = FactoryGirl.create(:animal)
+    animal.user_id = @user.id
+    visit("/animals/#{animal.id}")
+
+    expect(page).to have_content('Reksio')
+  end
+end
