91ca827349125b5295c1cfb889eb38375cb98cb5
kpietkaa <kpietkaa@gmail.com>
2016-12-06 01:05:52 +0100

    Add tests for animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,6 @@
 class Animal < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :name, :animal_type
+  validates_presence_of :name, :animal_type, :user
   enum animal_type: [
     :dog,
     :cat,
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    it 'requires name' do
+      animal = Animal.new(name: '')
+      animal.valid?
+      expect(animal.errors[:name]).not_to be_empty
+    end
+
+    it "allows different user to have animals with identical names" do
+      user1 = FactoryGirl.create(:user)
+      user2 = FactoryGirl.create(:user)
+      first_anmial = FactoryGirl.create(:animal, user: user1)
+      new_animal = FactoryGirl.create(:animal, user: user2)
+      expect(new_animal.valid?).to be_truthy
+    end
+
+    it 'belongs to user' do
+      animal = Animal.new(name: 'Reksio', animal_type: 1, user: nil)
+      expect(animal.valid?).to be_falsy
+    end
+
+    it 'has belongs_to user association' do
+      u = Animal.reflect_on_association(:user)
+      expect(u.macro).to eq(:belongs_to)
+    end
+  end
 end
