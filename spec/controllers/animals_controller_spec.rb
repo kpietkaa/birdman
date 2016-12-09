bdeb28a5be7d372d3588f6be85ecc9011b8df2f1
kpietkaa <kpietkaa@gmail.com>
2016-12-09 20:44:48 +0100

    Add some tests for animals controller

--- spec/controllers/animals_controller_spec.rb
+++ spec/controllers/animals_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
+require 'cancan/matchers'
 
 RSpec.describe AnimalsController, type: :controller do
+
+  describe "authenticated user" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in(user)
+    end
+
+    describe "GET index" do
+      it "renders :index template" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+    describe "GET new" do
+      it{ should be_able_to(:create, Animal.new) }
+    end
+
+    describe "not DELETE" do
+      it{ should_not be_able_to(:destroy, Animal.new) }
+    end
+
+    describe "DELETE own animals" do
+      it{ should be_able_to(:destroy, Animal.new(user: user)) }
+    end
+
+    describe "PUT own animal" do
+      it{ should be_able_to(:edit, Animal.new(user: user)) }
+    end
+  end
 
 end
